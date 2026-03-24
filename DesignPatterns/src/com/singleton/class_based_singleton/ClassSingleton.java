e5ce47b2a3eda7d031190daaa90c219b7e92df52
Minokainduwara <minokainduwara@gmail.com>
2026-03-24 23:18:02 +0530

    Feat: Class Base Singleton

--- DesignPatterns/src/com/singleton/class_based_singleton/ClassSingleton.java
+++ DesignPatterns/src/com/singleton/class_based_singleton/ClassSingleton.java
@@ -1,5 +1,24 @@
 package com.singleton.class_based_singleton;
-public class ClassSingleton {
+public final class ClassSingleton {
 	private static ClassSingleton INSTANCE;
     private String info = "Initial info class";
+
+    private ClassSingleton() {
+        // private constructor to prevent instantiation   
+    }
+
+    public static ClassSingleton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ClassSingleton();
+        }
+        return INSTANCE;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
